7824683153106255f3670ece96a9ae6eb34ce0fc
liujunlin <liujunlin@jrfreestyle.cn>
2020-01-10 17:55:45 +0800

    添加VUE优化方案

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,21 @@
-module.exports = {
+// 导入compression-webpack-plugin
+const CompressionWebpackPlugin = require('compression-webpack-plugin')
+// 定义压缩文件类型
+const productionGzipExtensions = ['js', 'css']
 
+module.exports = {
+	publicPath : process.env.NODE_ENV === 'production' ? '/hybridvue2/' : '/',
+	outputDir : process.env.NODE_ENV === 'production' ? 'hybridvue2' : 'dist',
+	lintOnSave: process.env.NODE_ENV !== 'production',
+	configureWebpack: {
+		plugins: [
+			new CompressionWebpackPlugin({
+				filename: '[path].gz[query]',
+				algorithm: 'gzip',
+				test: new RegExp('\\.(' + productionGzipExtensions.join('|') + ')$'),
+				threshold: 10240,
+				minRatio: 0.8
+			})
+		]
+	}
 }
